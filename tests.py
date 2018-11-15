6947afa81dfa6877fec58f9a9332a3d050e5cfde
zh_alex <zhdanoff.alexey@gmail.com>
2018-11-15 17:56:17 +0300

    issue smile

--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 import unittest
 
-import awesome
+import code
 
 
 class TestMethods(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(awesome.smile(), ":)")
+        self.assertEqual(code.smile(), ":)")
+        self.assertEqual(code.frown(), ":(")
 
 
 if __name__ == '__main__':
